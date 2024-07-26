e765850603415150eb9d4c6021ac30f4c791b14a
김지수 <wltn2858@swu.ac.kr>
2024-07-26 12:21:07 +0900

    :adhesive_bandage: Chore : response 데이터, KST 변환

--- src/main/java/com/parkro/server/domain/payment/dto/GetPaymentCouponRes.java
+++ src/main/java/com/parkro/server/domain/payment/dto/GetPaymentCouponRes.java
@@ -1,5 +1,7 @@
 package com.parkro.server.domain.payment.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.parkro.server.util.KSTDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +16,12 @@ import java.util.Date;
 public class GetPaymentCouponRes {
 
   private Integer memberCouponId;
-  private Integer memberId;
   private Integer couponId;
   private Integer parkingLotId;
   private String status;
+  @JsonSerialize(using = KSTDateSerializer.class)
   private Date createdDate;
   private Integer discountHour;
+  @JsonSerialize(using = KSTDateSerializer.class)
   private Date endDate; // 쿠폰 종료 기간
 }
