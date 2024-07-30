1b16363ad18ff987f7a7d493328ea69976f85dea
serak0310 <serak0310@naver.com>
2024-07-30 14:22:23 +0900

    :adhesive_bandage: Chore : firebase resource url yaml 설정

--- src/main/java/com/parkro/server/config/firebase/FCMConfig.java
+++ src/main/java/com/parkro/server/config/firebase/FCMConfig.java
@@ -4,10 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,9 +15,14 @@ import java.util.List;
 
 @Configuration
 public class FCMConfig {
+
+    @Value("${firebase.resource.url}")
+    private String keyPath;
+
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/serviceAccountKey.json");
+
+        ClassPathResource resource = new ClassPathResource(keyPath);
 
         InputStream refreshToken = resource.getInputStream();
 
