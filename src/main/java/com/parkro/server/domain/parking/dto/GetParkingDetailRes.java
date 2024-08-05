39076a649bb860250c7154c3aa8cd848731366bb
김지수 <wltn2858@swu.ac.kr>
2024-08-05 12:08:12 +0900

    :adhesive_bandage: Chore : 관리자 주차 상세 정보 조회 데이터 수정

--- src/main/java/com/parkro/server/domain/parking/dto/GetParkingDetailRes.java
+++ src/main/java/com/parkro/server/domain/parking/dto/GetParkingDetailRes.java
@@ -29,5 +29,7 @@ public class GetParkingDetailRes {
     private Integer couponDiscountTime;
     private Integer receiptDiscountTime;
     private Integer totalParkingTime;
+    private Integer parkingTimeHour;
+    private Integer parkingTimeMinute;
     private Integer totalPrice;
 }
