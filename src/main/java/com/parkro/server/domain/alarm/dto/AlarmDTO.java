a98a5e3626e1ca7468e1b7f5a8223a5a6b85a91f
serak0310 <serak0310@naver.com>
2024-08-09 17:29:17 +0900

    :memo: Comment : 주석 추가

--- src/main/java/com/parkro/server/domain/alarm/dto/AlarmDTO.java
+++ src/main/java/com/parkro/server/domain/alarm/dto/AlarmDTO.java
@@ -5,6 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 알람 베이스 DTO
+ *
+ * @author 김민정
+ * @since 2024.07.29
+ *
+ * <pre>
+ * 수정일자       수정자        수정내용
+ * ------------ --------    ---------------------------
+ * 2024.07.29   김민정       최초 생성
+ * </pre>
+ */
 @Getter
 @Builder
 @AllArgsConstructor
--- src/main/java/com/parkro/server/domain/alarm/dto/ParkingEntryDTO.java
+++ src/main/java/com/parkro/server/domain/alarm/dto/ParkingEntryDTO.java
@@ -5,6 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * 입차 알림 DTO
+ *
+ * @author 김민정
+ * @since 2024.07.29
+ *
+ * <pre>
+ * 수정일자       수정자        수정내용
+ * ------------ --------    ---------------------------
+ * 2024.07.29   김민정       최초 생성
+ * </pre>
+ */
 @Getter
 @Setter
 @AllArgsConstructor
